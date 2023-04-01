c33ece9bf2d33c119bae635d46a85cc919025089
karjudev <karjudev@gmail.com>
2023-04-01 19:01:48 +0200

    Refactored the model to use AllenNLP Light

--- model/ner_model.py
+++ model/ner_model.py
@@ -1,35 +1,39 @@
 from typing import List, Any
 from itertools import compress
 
-import pytorch_lightning.core.lightning as pl
+import lightning.pytorch as pl
 
 import torch
 import torch.nn.functional as F
 import numpy as np
 
-from allennlp.modules import ConditionalRandomField
-from allennlp.modules.conditional_random_field import allowed_transitions
+from allennlp_light import ConditionalRandomField
+from allennlp_light.modules.conditional_random_field.conditional_random_field import (
+    allowed_transitions,
+)
 from torch import nn
 from torch.utils.data import DataLoader
 from transformers import get_linear_schedule_with_warmup, AutoModel
+from data.spans import extract_spans
 
 from log import logger
 from utils.metric import SpanF1
-from utils.reader_utils import extract_spans, get_tags
 
 
 class NERBaseAnnotator(pl.LightningModule):
-    def __init__(self,
-                 train_data=None,
-                 dev_data=None,
-                 lr=1e-5,
-                 dropout_rate=0.1,
-                 batch_size=16,
-                 tag_to_id=None,
-                 stage='fit',
-                 pad_token_id=1,
-                 encoder_model='xlm-roberta-large',
-                 num_gpus=1):
+    def __init__(
+        self,
+        train_data=None,
+        dev_data=None,
+        lr=1e-5,
+        dropout_rate=0.1,
+        batch_size=16,
+        tag_to_id=None,
+        stage="fit",
+        pad_token_id=1,
+        encoder_model="xlm-roberta-large",
+        num_gpus=1,
+    ):
         super(NERBaseAnnotator, self).__init__()
 
         self.train_data = train_data
@@ -49,19 +53,29 @@ class NERBaseAnnotator(pl.LightningModule):
         self.encoder_model = encoder_model
         self.encoder = AutoModel.from_pretrained(encoder_model, return_dict=True)
 
-        self.feedforward = nn.Linear(in_features=self.encoder.config.hidden_size, out_features=self.target_size)
+        self.feedforward = nn.Linear(
+            in_features=self.encoder.config.hidden_size, out_features=self.target_size
+        )
 
-        self.crf_layer = ConditionalRandomField(num_tags=self.target_size, constraints=allowed_transitions(constraint_type="BIO", labels=self.id_to_tag))
+        self.crf_layer = ConditionalRandomField(
+            num_tags=self.target_size,
+            constraints=allowed_transitions(
+                constraint_type="BIO", labels=self.id_to_tag
+            ),
+        )
 
         self.lr = lr
         self.dropout = nn.Dropout(dropout_rate)
 
         self.span_f1 = SpanF1()
         self.setup_model(self.stage)
-        self.save_hyperparameters('pad_token_id', 'encoder_model')
+        self.save_hyperparameters("pad_token_id", "encoder_model")
+        self.training_outputs = []
+        self.validation_outputs = []
+        self.testing_outputs = []
 
     def setup_model(self, stage_name):
-        if stage_name == 'fit' and self.train_data is not None:
+        if stage_name == "fit" and self.train_data is not None:
             # Calculate total steps
             train_batches = len(self.train_data) // (self.batch_size * self.num_gpus)
             self.total_steps = 50 * train_batches
@@ -70,11 +84,21 @@ class NERBaseAnnotator(pl.LightningModule):
 
     def collate_batch(self, batch):
         batch_ = list(zip(*batch))
-        tokens, masks, token_masks, gold_spans, tags = batch_[0], batch_[1], batch_[2], batch_[3], batch_[4]
+        tokens, masks, token_masks, gold_spans, tags = (
+            batch_[0],
+            batch_[1],
+            batch_[2],
+            batch_[3],
+            batch_[4],
+        )
 
         max_len = max([len(token) for token in tokens])
-        token_tensor = torch.empty(size=(len(tokens), max_len), dtype=torch.long).fill_(self.pad_token_id)
-        tag_tensor = torch.empty(size=(len(tokens), max_len), dtype=torch.long).fill_(self.tag_to_id['O'])
+        token_tensor = torch.empty(size=(len(tokens), max_len), dtype=torch.long).fill_(
+            self.pad_token_id
+        )
+        tag_tensor = torch.empty(size=(len(tokens), max_len), dtype=torch.long).fill_(
+            self.tag_to_id["O"]
+        )
         mask_tensor = torch.zeros(size=(len(tokens), max_len), dtype=torch.bool)
         token_masks_tensor = torch.zeros(size=(len(tokens), max_len), dtype=torch.bool)
 
@@ -91,66 +115,119 @@ class NERBaseAnnotator(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=0.01)
-        if self.stage == 'fit':
-            scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=self.warmup_steps, num_training_steps=self.total_steps)
-            scheduler = {
-                'scheduler': scheduler,
-                'interval': 'step',
-                'frequency': 1
-            }
+        if self.stage == "fit":
+            scheduler = get_linear_schedule_with_warmup(
+                optimizer,
+                num_warmup_steps=self.warmup_steps,
+                num_training_steps=self.total_steps,
+            )
+            scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
             return [optimizer], [scheduler]
         return [optimizer]
 
     def train_dataloader(self):
-        loader = DataLoader(self.train_data, batch_size=self.batch_size, collate_fn=self.collate_batch, num_workers=10)
+        loader = DataLoader(
+            self.train_data,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_batch,
+            num_workers=10,
+        )
         return loader
 
     def val_dataloader(self):
         if self.dev_data is None:
             return None
-        loader = DataLoader(self.dev_data, batch_size=self.batch_size, collate_fn=self.collate_batch, num_workers=10)
+        loader = DataLoader(
+            self.dev_data,
+            batch_size=self.batch_size,
+            collate_fn=self.collate_batch,
+            num_workers=10,
+        )
         return loader
 
-    def test_epoch_end(self, outputs):
+    def on_test_epoch_end(self):
         pred_results = self.span_f1.get_metric()
-        avg_loss = np.mean([preds['loss'].item() for preds in outputs])
+        avg_loss = np.mean([preds["loss"].item() for preds in self.testing_outputs])
         self.log_metrics(pred_results, loss=avg_loss, on_step=False, on_epoch=True)
-
         out = {"test_loss": avg_loss, "results": pred_results}
+        self.testing_outputs.clear()
         return out
 
-    def training_epoch_end(self, outputs: List[Any]) -> None:
+    def on_training_epoch_end(self) -> None:
         pred_results = self.span_f1.get_metric(True)
-        avg_loss = np.mean([preds['loss'].item() for preds in outputs])
-        self.log_metrics(pred_results, loss=avg_loss, suffix='', on_step=False, on_epoch=True)
+        avg_loss = np.mean([preds["loss"].item() for preds in self.training_outputs])
+        self.log_metrics(
+            pred_results, loss=avg_loss, suffix="", on_step=False, on_epoch=True
+        )
+        self.training_outputs.clear()
 
-    def validation_epoch_end(self, outputs: List[Any]) -> None:
+    def on_validation_epoch_end(self) -> None:
         pred_results = self.span_f1.get_metric(True)
-        avg_loss = np.mean([preds['loss'].item() for preds in outputs])
-        self.log_metrics(pred_results, loss=avg_loss, suffix='val_', on_step=False, on_epoch=True)
+        avg_loss = np.mean([preds["loss"].item() for preds in self.validation_outputs])
+        self.log_metrics(
+            pred_results, loss=avg_loss, suffix="val_", on_step=False, on_epoch=True
+        )
+        self.validation_outputs.clear()
 
     def validation_step(self, batch, batch_idx):
         output = self.perform_forward_step(batch)
-        self.log_metrics(output['results'], loss=output['loss'], suffix='val_', on_step=True, on_epoch=False)
+        self.log_metrics(
+            output["results"],
+            loss=output["loss"],
+            suffix="val_",
+            on_step=True,
+            on_epoch=False,
+        )
+        self.validation_outputs.append(output)
         return output
 
     def training_step(self, batch, batch_idx):
         output = self.perform_forward_step(batch)
-        self.log_metrics(output['results'], loss=output['loss'], suffix='', on_step=True, on_epoch=False)
+        self.log_metrics(
+            output["results"],
+            loss=output["loss"],
+            suffix="",
+            on_step=True,
+            on_epoch=False,
+        )
+        self.training_outputs.append(output)
         return output
 
     def test_step(self, batch, batch_idx):
         output = self.perform_forward_step(batch, mode=self.stage)
-        self.log_metrics(output['results'], loss=output['loss'], suffix='_t', on_step=True, on_epoch=False)
+        self.log_metrics(
+            output["results"],
+            loss=output["loss"],
+            suffix="_t",
+            on_step=True,
+            on_epoch=False,
+        )
+        self.testing_outputs.append(output)
         return output
 
-    def log_metrics(self, pred_results, loss=0.0, suffix='', on_step=False, on_epoch=True):
+    def log_metrics(
+        self, pred_results, loss=0.0, suffix="", on_step=False, on_epoch=True
+    ):
         for key in pred_results:
-            self.log(suffix + key, pred_results[key], on_step=on_step, on_epoch=on_epoch, prog_bar=True, logger=True)
+            self.log(
+                suffix + key,
+                pred_results[key],
+                on_step=on_step,
+                on_epoch=on_epoch,
+                prog_bar=True,
+                logger=True,
+            )
 
-        self.log(suffix + 'loss', loss, on_step=on_step, on_epoch=on_epoch, prog_bar=True, logger=True)
+        self.log(
+            suffix + "loss",
+            loss,
+            on_step=on_step,
+            on_epoch=on_epoch,
+            prog_bar=True,
+            logger=True,
+        )
 
-    def perform_forward_step(self, batch, mode=''):
+    def perform_forward_step(self, batch, mode=""):
         tokens, tags, mask, token_mask, metadata = batch
         batch_size = tokens.size(0)
 
@@ -163,10 +240,19 @@ class NERBaseAnnotator(pl.LightningModule):
         token_scores = F.log_softmax(token_scores, dim=-1)
 
         # compute the log-likelihood loss and compute the best NER annotation sequence
-        output = self._compute_token_tags(token_scores=token_scores, mask=mask, tags=tags, metadata=metadata, batch_size=batch_size, mode=mode)
+        output = self._compute_token_tags(
+            token_scores=token_scores,
+            mask=mask,
+            tags=tags,
+            metadata=metadata,
+            batch_size=batch_size,
+            mode=mode,
+        )
         return output
 
-    def _compute_token_tags(self, token_scores, mask, tags, metadata, batch_size, mode=''):
+    def _compute_token_tags(
+        self, token_scores, mask, tags, metadata, batch_size, mode=""
+    ):
         # compute the log-likelihood loss and compute the best NER annotation sequence
         loss = -self.crf_layer(token_scores, tags, mask) / float(batch_size)
         best_path = self.crf_layer.viterbi_tags(token_scores, mask)
@@ -175,20 +261,28 @@ class NERBaseAnnotator(pl.LightningModule):
         for i in range(batch_size):
             tag_seq, _ = best_path[i]
             pred_tags.append([self.id_to_tag[x] for x in tag_seq])
-            pred_results.append(extract_spans([self.id_to_tag[x] for x in tag_seq if x in self.id_to_tag]))
+            pred_results.append(extract_spans(tag_seq, self.id_to_tag))
 
         self.span_f1(pred_results, metadata)
         output = {"loss": loss, "results": self.span_f1.get_metric()}
 
-        if mode == 'predict':
-            output['token_tags'] = pred_tags
+        if mode == "predict":
+            output["token_tags"] = pred_tags
         return output
 
-    def predict_tags(self, batch, device='cuda:0'):
+    def predict_tags(self, batch, device="cuda:0"):
         tokens, tags, mask, token_mask, metadata = batch
-        tokens, mask, token_mask, tags = tokens.to(device), mask.to(device), token_mask.to(device), tags.to(device)
+        tokens, mask, token_mask, tags = (
+            tokens.to(device),
+            mask.to(device),
+            token_mask.to(device),
+            tags.to(device),
+        )
         batch = tokens, tags, mask, token_mask, metadata
 
-        pred_tags = self.perform_forward_step(batch, mode='predict')['token_tags']
-        tag_results = [compress(pred_tags_, mask_) for pred_tags_, mask_ in zip(pred_tags, token_mask)]
+        pred_tags = self.perform_forward_step(batch, mode="predict")["token_tags"]
+        tag_results = [
+            compress(pred_tags_, mask_)
+            for pred_tags_, mask_ in zip(pred_tags, token_mask)
+        ]
         return tag_results
