89e8a6fbfda8ee67d35c9dd3783780731ed7cf10
karjudev <karjudev@gmail.com>
2023-04-03 15:29:33 +0200

    Customizer training logic from `utils`

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,14 @@
 import os
+from pathlib import Path
 import time
+from typing import Any, Mapping, Tuple
 
 import torch
 from lightning.pytorch import seed_everything, Trainer
 from lightning.pytorch.callbacks import LearningRateMonitor, EarlyStopping
 import optuna
 from optuna.pruners import MedianPruner
+from data.ordinances import OrdinancesDataModule
 
 from log import logger
 from model.callbacks import PyTorchLightningPruningCallback
@@ -30,31 +33,6 @@ def write_eval_performance(eval_performance, out_file):
 
     open(out_file, "wt").write(outstr)
     logger.info("Finished writing evaluation performance for {}".format(out_file))
-
-
-def create_model(
-    train_data,
-    dev_data,
-    tag_to_id,
-    batch_size=64,
-    dropout_rate=0.1,
-    stage="fit",
-    lr=1e-5,
-    encoder_model="xlm-roberta-large",
-    num_gpus=1,
-):
-    return NERBaseAnnotator(
-        train_data=train_data,
-        dev_data=dev_data,
-        tag_to_id=tag_to_id,
-        batch_size=batch_size,
-        stage=stage,
-        encoder_model=encoder_model,
-        dropout_rate=dropout_rate,
-        lr=lr,
-        pad_token_id=train_data.pad_token_id,
-        num_gpus=num_gpus,
-    )
 
 
 def load_model(model_file, tag_to_id=None, stage="test"):
@@ -84,89 +62,83 @@ def save_model(trainer, out_dir, model_name="", timestamp=None):
     return outfile
 
 
-def train_model(model, out_dir=None, epochs=10, gpus=1, trial=None):
-    trainer = get_trainer(gpus=gpus, out_dir=out_dir, epochs=epochs, trial=trial)
-    trainer.fit(model)
+def train_model(
+    model: NERBaseAnnotator,
+    datamodule: OrdinancesDataModule,
+    epochs: int,
+    out_dir: Path,
+    metric: str = "val_MD@F1",
+) -> Trainer:
+    seed_everything(42)
+    es_callback = EarlyStopping(
+        monitor=metric, min_delta=0.001, patience=3, verbose=True, mode="max"
+    )
+    lr_logger = LearningRateMonitor(logging_interval="step")
+    trainer = Trainer(
+        max_epochs=epochs, default_root_dir=out_dir, callbacks=[es_callback, lr_logger]
+    )
+    trainer.fit(model, datamodule=datamodule)
     return trainer
 
 
 def tune_model(
-    train_data,
-    tune_data,
-    encoder_model,
-    tag_to_id,
-    batch_size,
-    dropout_rate,
-    epochs,
-    n_trials=10,
-):
+    datamodule: OrdinancesDataModule,
+    encoder_model: str,
+    epochs: int,
+    dropout_rate: float,
+    n_trials: int = 10,
+    metric: str = "val_MD@F1",
+) -> Tuple[float, Mapping[str, Any]]:
+    seed_everything(42)
+    # Training and validation data
+    tag_to_id = datamodule.tag_to_id
+    train_data = datamodule.train_dataloader()
+    tune_data = datamodule.tune_dataloader()
+    # Number of training and warm-up steps
+    num_training_steps, num_warmup_steps = datamodule.num_training_steps(epochs)
+
     def objective(trial):
+        # Extracts hyperparameters
         lr = trial.suggest_float("lr", low=1e-5, high=5e-4)
-        hyperparameters = {"lr": lr}
-        model = create_model(
-            train_data,
-            tune_data,
+        weight_decay = trial.suggest_float("weight_decay", low=0.01, high=0.1)
+        dropout_rate = trial.suggest_float("dropout_rate", low=0.01, high=0.1)
+        grad_norm = trial.suggest_float("grad_norm", low=1.0, high=5.0)
+        hyperparameters = {
+            "lr": lr,
+            "weight_decay": weight_decay,
+            "dropout_rate": dropout_rate,
+            "grad_norm": grad_norm,
+        }
+        # Creates the model
+        model = NERBaseAnnotator(
+            encoder_model,
             tag_to_id,
-            batch_size,
+            lr,
+            num_training_steps,
+            num_warmup_steps,
             dropout_rate,
             stage="tuning",
-            lr=lr,
-            encoder_model=encoder_model,
+            weight_decay=weight_decay,
         )
-        trainer = train_model(model=model, epochs=epochs, trial=trial)
+        # Creates the trainer
+        pruning_callback = PyTorchLightningPruningCallback(trial, monitor=metric)
+        lr_logger = LearningRateMonitor(logging_interval="step")
+        trainer = Trainer(
+            max_epochs=epochs,
+            enable_checkpointing=False,
+            callbacks=[pruning_callback, lr_logger],
+            gradient_clip_algorithm="norm",
+            gradient_clip_val=grad_norm,
+        )
         trainer.logger.log_hyperparams(hyperparameters)
+        # Fits the model
+        trainer.fit(model, train_dataloaders=train_data, val_dataloaders=tune_data)
+        # Returns the best metric
         return trainer.logged_metrics["val_MD@F1"].item()
 
     study = optuna.create_study(direction="maximize", pruner=MedianPruner())
     study.optimize(objective, n_trials=n_trials)
     return study.best_value, study.best_params
-
-
-def get_trainer(gpus=4, is_test=False, out_dir=None, epochs=10, trial=None):
-    enable_checkpointing = out_dir is not None
-    seed_everything(42)
-    if is_test:
-        return (
-            Trainer(devices=1, enable_checkpointing=enable_checkpointing)
-            if torch.cuda.is_available()
-            else Trainer(
-                val_check_interval=100, enable_checkpointing=enable_checkpointing
-            )
-        )
-
-    if torch.cuda.is_available():
-        trainer = Trainer(
-            devices=gpus,
-            deterministic=True,
-            max_epochs=epochs,
-            callbacks=[get_model_earlystopping_callback(trial)],
-            default_root_dir=out_dir,
-            enable_checkpointing=enable_checkpointing,
-        )
-        trainer.callbacks.append(get_lr_logger())
-    else:
-        trainer = Trainer(
-            max_epochs=epochs,
-            enable_checkpointing=enable_checkpointing,
-            default_root_dir=out_dir,
-        )
-
-    return trainer
-
-
-def get_lr_logger():
-    lr_monitor = LearningRateMonitor(logging_interval="step")
-    return lr_monitor
-
-
-def get_model_earlystopping_callback(trial=None, metric="val_MD@F1"):
-    if trial is None:
-        es_clb = EarlyStopping(
-            monitor=metric, min_delta=0.001, patience=3, verbose=True, mode="min"
-        )
-    else:
-        es_clb = PyTorchLightningPruningCallback(trial, monitor=metric)
-    return es_clb
 
 
 def get_models_for_evaluation(path):
